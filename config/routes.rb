7c82959ab6907daf8ffc272adb497a7f8c51f392
Peyton Kellogg <pekellogg@hotmail.com>
2022-06-11 03:04:07 -0700

    successfully generated self-signed cert

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+    # OAuth APIs
+    match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+    
+    # User
+    root(to: 'welcome#home')
+    get '/users/home', to: 'welcome#home'
 end
