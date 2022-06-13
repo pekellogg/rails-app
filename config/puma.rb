cc0f0f0d3f9847494af6321f9edabfdef207e3c8
Peyton Kellogg <pekellogg@hotmail.com>
2022-06-13 12:25:04 -0700

    commiting pre-Gemfile updates using Bundler's gem update 'puma' command

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,7 @@
-# require "nakayoshi_fork"
-
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads min_threads_count, max_threads_count
-
-# nakayoshi_fork
-# wait_for_less_busy_worker
-# fork_worker
 
 rails_env = ENV.fetch('RAILS_ENV', 'development')
 rails_port = ENV.fetch('PORT', 3000)
@@ -27,6 +21,7 @@ else
 end
 
 # Allow puma to be restarted by `rails restart` command.
+#=> https://www.filippoliverani.com/ssl-rails-local-development-puma
 plugin :tmp_restart
 
 preload_app!
